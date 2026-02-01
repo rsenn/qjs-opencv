9c826c06e74892f1bed6ab55eff2bbf34ae8b5a9
Roman Senn <roman.l.senn@gmail.com>
2026-02-01 09:28:05 +0100

    ...

--- tests/test_video_homography.js
+++ tests/test_video_homography.js
@@ -154,7 +154,7 @@ function main(cam = 0) {
 
         const nz = countNonZero(match_mask);
 
-        console.log('H', H);
+        console.log('H', H.array);
 
         if(nz > 15) H_prev = H;
         else resetH();
