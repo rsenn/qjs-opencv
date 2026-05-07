b4c09e7528a56a47c76feae345452e6ca9d11968
Roman Senn <roman.l.senn@gmail.com>
2026-05-07 21:34:14 +0200

    ...

--- js_dnn.cpp
+++ js_dnn.cpp
@@ -5,43 +5,52 @@
 
 #include <opencv2/dnn.hpp>
 
+using JSNetData = cv::dnn::Net;
+
 extern "C" {
-thread_local JSValue net_proto, net_class;
+thread_local JSValue dnn_object, net_proto, net_class;
 thread_local JSClassID js_net_class_id;
 }
 
 static JSValue
+js_net_wrap(JSContext* ctx, JSValueConst proto, JSNetData* fn) {
+  JSValue ret = JS_NewObjectProtoClass(ctx, proto, js_net_class_id);
+  JS_SetOpaque(ret, fn);
+  return ret;
+}
+
+JSValue
+js_net_new(JSContext* ctx, JSValueConst proto) {
+  JSNetData* fn = js_allocate<JSNetData>(ctx);
+
+  new(fn) JSNetData();
+
+  return js_net_wrap(ctx, proto, fn);
+}
+
+JSValue
+js_net_new(JSContext* ctx, JSValueConst proto, const JSNetData& other) {
+  JSNetData* fn = js_allocate<JSNetData>(ctx);
+
+  new(fn) JSNetData(other);
+
+  return js_net_wrap(ctx, proto, fn);
+}
+
+JSValue
+js_net_new(JSContext* ctx, const JSNetData& fn) {
+  return js_net_new(ctx, net_proto, fn);
+}
+
+static JSValue
 js_net_constructor(JSContext* ctx, JSValueConst new_target, int argc, JSValueConst argv[]) {
-  JSNetData* fs;
+  JSNetData* dn;
   JSValue obj = JS_UNDEFINED, proto;
 
-  if(!(fs = js_allocate<JSNetData>(ctx)))
+  if(!(dn = js_allocate<JSNetData>(ctx)))
     return JS_EXCEPTION;
 
-  if(argc == 0) {
-    new(fs) JSNetData();
-  } else {
-    const char *filename = 0, *encoding = 0;
-    int32_t flags = 0;
-
-    if(!(filename = JS_ToCString(ctx, argv[0])))
-      goto fail;
-
-    if(argc > 1)
-      JS_ToInt32(ctx, &flags, argv[1]);
-
-    if(argc > 2)
-      encoding = JS_ToCString(ctx, argv[2]);
-
-    if(encoding)
-      new(fs) JSNetData(filename, flags, encoding);
-    else
-      new(fs) JSNetData(filename, flags);
-
-    JS_FreeCString(ctx, filename);
-    if(encoding)
-      JS_FreeCString(ctx, encoding);
-  }
+  new(dn) JSNetData();
 
   /* using new_target to get the prototype is necessary when the class is extended. */
   proto = JS_GetPropertyStr(ctx, new_target, "prototype");
@@ -54,12 +63,12 @@ js_net_constructor(JSContext* ctx, JSValueConst new_target, int argc, JSValueCon
   if(JS_IsException(obj))
     goto fail;
 
-  JS_SetOpaque(obj, fs);
+  JS_SetOpaque(obj, dn);
 
   return obj;
 
 fail:
-  js_deallocate(ctx, fs);
+  js_deallocate(ctx, dn);
   JS_FreeValue(ctx, obj);
   return JS_EXCEPTION;
 }
@@ -74,351 +83,36 @@ js_net_data2(JSContext* ctx, JSValueConst val) {
   return static_cast<JSNetData*>(JS_GetOpaque2(ctx, val, js_net_class_id));
 }
 
-static JSValue
-js_net_get(JSContext* ctx, JSValueConst this_val, int magic) {
-  JSNetData* fs;
-  JSValue ret = JS_UNDEFINED;
-
-  if(!(fs = js_net_data2(ctx, this_val)))
-    return JS_EXCEPTION;
-
-  switch(magic) {}
-
-  return ret;
-}
-
-static JSValue
-js_net_set(JSContext* ctx, JSValueConst this_val, JSValueConst val, int magic) {
-  JSNetData* fs;
-
-  if(!(fs = js_net_data2(ctx, this_val)))
-    return JS_EXCEPTION;
-
-  switch(magic) {}
-
-  return JS_UNDEFINED;
-}
-
 enum {
-  FUNCTION_GETDEFAULTOBJECTNAME,
-};
-
-static JSValue
-js_net_funcs(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst argv[], int magic) {
-  JSValue ret = JS_EXCEPTION;
-
-  switch(magic) {
-    case FUNCTION_GETDEFAULTOBJECTNAME: {
-      const char* filename;
-
-      if(!(filename = JS_ToCString(ctx, argv[0])))
-        return JS_ThrowTypeError(ctx, "argument 1 must be a string");
-
-      std::string str = cv::Net::getDefaultObjectName(filename);
-
-      JS_FreeCString(ctx, filename);
-
-      ret = JS_NewString(ctx, str.c_str());
-      break;
-    }
-  }
-
-  return ret;
-}
-
-enum { GLOBAL_WRITE, GLOBAL_WRITESCALAR };
-
-static JSValue
-js_net_global(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst argv[], int magic) {
-  JSValue ret = JS_EXCEPTION;
-  JSNetData* fs;
-
-  if(!(fs = js_net_data2(ctx, argv[0])))
-    return JS_EXCEPTION;
-
-  switch(magic) {
-    case GLOBAL_WRITE: {
-      if(argc > 2) {
-        const char* name;
-
-        if(!(name = JS_ToCString(ctx, argv[1])))
-          return JS_ThrowTypeError(ctx, "argument 2 must be a string");
-
-        JSMatData* m;
-
-        if((m = js_mat_data(argv[2]))) {
-          cv::write(*fs, name, *m);
-        } else if(JS_IsNumber(argv[2])) {
-          double val;
-          JS_ToFloat64(ctx, &val, argv[2]);
-
-          cv::write(*fs, name, val);
-        } else if(JS_IsString(argv[2])) {
-          const char* str;
-
-          if(!(str = JS_ToCString(ctx, argv[2])))
-            return JS_ThrowTypeError(ctx, "argument 3 must be a string");
-
-          cv::write(*fs, name, str);
-          JS_FreeCString(ctx, str);
-        }
-
-        JS_FreeCString(ctx, name);
-      } else {
-        JSPointData<double> pt;
-        JSSizeData<double> sz;
-        JSRectData<double> rt;
-
-        if(JS_IsNumber(argv[1])) {
-          double val;
-          JS_ToFloat64(ctx, &val, argv[1]);
-
-          cv::write(*fs, val);
-        } else if(JS_IsString(argv[1])) {
-          const char* str;
-
-          if(!(str = JS_ToCString(ctx, argv[1])))
-            return JS_ThrowTypeError(ctx, "argument 2 must be a string");
-
-          cv::write(*fs, str);
-          JS_FreeCString(ctx, str);
-        } else if(js_point_read(ctx, argv[1], &pt)) {
-          cv::write(*fs, pt);
-        } else if(js_size_read(ctx, argv[1], &sz)) {
-          cv::write(*fs, sz);
-        } else if(js_rect_read(ctx, argv[1], &rt)) {
-          cv::write(*fs, rt);
-        }
-      }
-
-      break;
-    }
-
-    case GLOBAL_WRITESCALAR: {
-      if(JS_IsNumber(argv[1])) {
-        double val;
-        JS_ToFloat64(ctx, &val, argv[1]);
-
-        cv::writeScalar(*fs, val);
-      } else {
-        const char* str;
-
-        if(!(str = JS_ToCString(ctx, argv[1])))
-          return JS_ThrowTypeError(ctx, "argument 2 must be a string");
-
-        cv::writeScalar(*fs, str);
-        JS_FreeCString(ctx, str);
-      }
-
-      break;
-    }
-  }
-
-  return ret;
-}
-
-enum {
-  METHOD_OPEN,
-  METHOD_RELEASE,
-  METHOD_RELEASEANDGETSTRING,
-  METHOD_ENDWRITESTRUCT,
-  METHOD_GETFIRSTTOPLEVELNODE,
-  METHOD_GETFORMAT,
-  METHOD_ISOPENED,
-  METHOD_ROOT,
-  METHOD_STARTWRITESTRUCT,
-  METHOD_WRITE,
-  METHOD_WRITECOMMENT,
-  METHOD_WRITERAW,
-  METHOD_GETNODE,
+  DNN_NET_FORWARD,
+  DNN_NET_GETUNCONNECTEDOUTLAYERSNAMES,
+  DNN_NET_SETINPUT,
+  DNN_NET_SETPREFERABLEBACKEND,
+  DNN_NET_SETPREFERABLETARGET,
 };
 
 static JSValue
 js_net_method(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst argv[], int magic) {
-  JSNetData* fs;
+  JSNetData* dn;
   JSValue ret = JS_UNDEFINED;
 
-  if(!(fs = js_net_data2(ctx, this_val)))
+  if(!(dn = js_net_data2(ctx, this_val)))
     return JS_EXCEPTION;
 
-  switch(magic) {
-    case METHOD_OPEN: {
-      const char *filename, *encoding = 0;
-      int32_t flags = 0;
-
-      if(!(filename = JS_ToCString(ctx, argv[0])))
-        return JS_ThrowTypeError(ctx, "argument 1 must be a string");
-
-      if(argc > 1)
-        JS_ToInt32(ctx, &flags, argv[1]);
-
-      if(argc > 2)
-        encoding = JS_ToCString(ctx, argv[2]);
-
-      ret = JS_NewBool(ctx, fs->open(filename, flags, encoding));
-
-      JS_FreeCString(ctx, filename);
-      if(encoding)
-        JS_FreeCString(ctx, encoding);
-
-      break;
-    }
-
-    case METHOD_RELEASE: {
-      fs->release();
-      break;
-    }
-
-    case METHOD_RELEASEANDGETSTRING: {
-      std::string str = fs->releaseAndGetString();
-
-      ret = JS_NewString(ctx, str.c_str());
-      break;
-    }
-
-    case METHOD_ENDWRITESTRUCT: {
-      fs->endWriteStruct();
-      break;
-    }
-
-    case METHOD_GETFIRSTTOPLEVELNODE: {
-      ret = js_filenode_new(ctx, fs->getFirstTopLevelNode());
-      break;
-    }
-
-    case METHOD_GETFORMAT: {
-      ret = JS_NewInt32(ctx, fs->getFormat());
-      break;
-    }
-
-    case METHOD_GETNODE: {
-      const char* name = JS_ToCString(ctx, argv[0]);
-
-      ret = js_filenode_new(ctx, fs->operator[](name));
-
-      if(name)
-        JS_FreeCString(ctx, name);
-
-      break;
-    }
-
-    case METHOD_ISOPENED: {
-      ret = JS_NewBool(ctx, fs->isOpened());
-      break;
-    }
-
-    case METHOD_ROOT: {
-      int32_t idx = 0;
-
-      if(argc > 0)
-        JS_ToInt32(ctx, &idx, argv[0]);
-
-      ret = js_filenode_new(ctx, fs->root());
-      break;
-    }
-
-    case METHOD_STARTWRITESTRUCT: {
-      const char *name, *typeName;
-      int32_t flags = 0;
-
-      if(!(name = JS_ToCString(ctx, argv[0])))
-        return JS_ThrowTypeError(ctx, "argument 1 must be a string");
-
-      if(argc > 1)
-        JS_ToInt32(ctx, &flags, argv[1]);
-
-      if(argc > 2)
-        typeName = JS_ToCString(ctx, argv[2]);
-
-      fs->startWriteStruct(name, flags, typeName);
-
-      JS_FreeCString(ctx, name);
-      if(typeName)
-        JS_FreeCString(ctx, typeName);
-      break;
-    }
-
-    case METHOD_WRITE: {
-      const char* name;
-
-      if(!(name = JS_ToCString(ctx, argv[0])))
-        return JS_ThrowTypeError(ctx, "argument 1 must be a string");
-
-      JSMatData* m;
-
-      if(js_is_array(ctx, argv[1])) {
-        std::vector<std::string> strv;
-
-        js_array_to(ctx, argv[1], strv);
-
-        fs->write(name, strv);
-      } else if((m = js_mat_data(argv[1]))) {
-        fs->write(name, *m);
-
-      } else if(JS_IsNumber(argv[1])) {
-        double val;
-        JS_ToFloat64(ctx, &val, argv[1]);
-
-        fs->write(name, val);
-      } else if(JS_IsString(argv[1])) {
-        const char* str;
-
-        if(!(str = JS_ToCString(ctx, argv[1])))
-          return JS_ThrowTypeError(ctx, "argument 2 must be a string");
-
-        fs->write(name, str);
-        JS_FreeCString(ctx, str);
-      }
-
-      break;
-    }
-
-    case METHOD_WRITECOMMENT: {
-      const char* comment;
-
-      if(!(comment = JS_ToCString(ctx, argv[0])))
-        return JS_ThrowTypeError(ctx, "argument 1 must be a string");
-
-      BOOL append = FALSE;
-
-      if(argc > 1)
-        append = JS_ToBool(ctx, argv[1]);
-
-      fs->writeComment(comment, append);
-      break;
-    }
-
-    case METHOD_WRITERAW: {
-      const char* fmt;
-
-      if(!(fmt = JS_ToCString(ctx, argv[0])))
-        return JS_ThrowTypeError(ctx, "argument 1 must be a string");
-
-      uint8_t* buf;
-      size_t len;
-
-      if(!(buf = JS_GetArrayBuffer(ctx, &len, argv[1]))) {
-        JS_FreeCString(ctx, fmt);
-        return JS_ThrowTypeError(ctx, "argument 2 must be an ArrayBuffer");
-      }
-
-      fs->writeRaw(fmt, buf, len);
-      break;
-    }
-  }
+  switch(magic) {}
 
   return ret;
 }
 
 void
 js_net_finalizer(JSRuntime* rt, JSValue val) {
-  JSNetData* fs;
-  /* Note: 'fs' can be NULL in case JS_SetOpaque() was not called */
+  JSNetData* dn;
+  /* Note: 'dn' can be NULL in case JS_SetOpaque() was not called */
 
-  if((fs = js_net_data(val))) {
-    fs->~JSNetData();
+  if((dn = js_net_data(val))) {
+    dn->~JSNetData();
 
-    js_deallocate(rt, fs);
+    js_deallocate(rt, dn);
   }
 }
 
@@ -428,28 +122,16 @@ JSClassDef js_net_class = {
 };
 
 const JSCFunctionListEntry js_net_proto_funcs[] = {
-    JS_CFUNC_MAGIC_DEF("open", 2, js_net_method, METHOD_OPEN),
-    JS_CFUNC_MAGIC_DEF("release", 0, js_net_method, METHOD_RELEASE),
-
-    JS_CFUNC_MAGIC_DEF("endWriteStruct", 0, js_net_method, METHOD_ENDWRITESTRUCT),
-    JS_CFUNC_MAGIC_DEF("getFirstTopLevelNode", 0, js_net_method, METHOD_GETFIRSTTOPLEVELNODE),
-    JS_CFUNC_MAGIC_DEF("getFormat", 0, js_net_method, METHOD_GETFORMAT),
-    JS_CFUNC_MAGIC_DEF("isOpened", 0, js_net_method, METHOD_ISOPENED),
-    JS_CFUNC_MAGIC_DEF("releaseAndGetString", 0, js_net_method, METHOD_RELEASEANDGETSTRING),
-    JS_CFUNC_MAGIC_DEF("root", 0, js_net_method, METHOD_ROOT),
-    JS_CFUNC_MAGIC_DEF("startWriteStruct", 2, js_net_method, METHOD_STARTWRITESTRUCT),
-    JS_CFUNC_MAGIC_DEF("write", 2, js_net_method, METHOD_WRITE),
-    JS_CFUNC_MAGIC_DEF("writeComment", 0, js_net_method, METHOD_WRITECOMMENT),
-    JS_CFUNC_MAGIC_DEF("writeRaw", 2, js_net_method, METHOD_WRITERAW),
-
-    JS_CFUNC_MAGIC_DEF("getNode", 1, js_net_method, METHOD_GETNODE),
+    JS_CFUNC_MAGIC_DEF("forward", 0, js_net_method, DNN_NET_FORWARD),
+    JS_CFUNC_MAGIC_DEF("getUnconnectedOutLayersNames", 0, js_net_method, DNN_NET_GETUNCONNECTEDOUTLAYERSNAMES),
+    JS_CFUNC_MAGIC_DEF("setInput", 0, js_net_method, DNN_NET_SETINPUT),
+    JS_CFUNC_MAGIC_DEF("setPreferableBackend", 0, js_net_method, DNN_NET_SETPREFERABLEBACKEND),
+    JS_CFUNC_MAGIC_DEF("setPreferableTarget", 0, js_net_method, DNN_NET_SETPREFERABLETARGET),
 
     JS_PROP_STRING_DEF("[Symbol.toStringTag]", "Net", JS_PROP_CONFIGURABLE),
 };
 
-const JSCFunctionListEntry js_net_static_funcs[] = {
-    JS_CFUNC_MAGIC_DEF("getDefaultObjectName", 1, js_net_funcs, FUNCTION_GETDEFAULTOBJECTNAME),
-};
+const JSCFunctionListEntry js_net_static_funcs[] = {};
 
 enum {
   DNN_LAYOUT_NCHW,
@@ -465,7 +147,41 @@ js_dnn_func(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst argv[]
   JSValue ret = JS_UNDEFINED;
 
   try {
-    switch(magic) {}
+    switch(magic) {
+
+      case DNN_LAYOUT_NCHW: {
+        break;
+      }
+      case DNN_IMAGE2BLOBPARAMS: {
+        break;
+      }
+      case DNN_IMAGEPADDINGMODE: {
+        break;
+      }
+      case DNN_NET: {
+        break;
+      }
+      case DNN_NMSBOXES: {
+        break;
+      }
+      case DNN_READNET: {
+        const char *model, *config, *framework;
+
+        if(argc > 0)
+          model = JS_ToCString(ctx, argv[0]);
+
+        if(argc > 1)
+          config = JS_ToCString(ctx, argv[1]);
+
+        if(argc > 2)
+          framework = JS_ToCString(ctx, argv[2]);
+
+        JSNetData n = cv::dnn::readNet(model, config ? config : "", framework ? framework : "");
+
+        ret = js_net_new(ctx, net_proto, n);
+        break;
+      }
+    }
   } catch(const cv::Exception& e) { ret = js_cv_throw(ctx, e); }
 
   return ret;
@@ -487,6 +203,7 @@ const JSCFunctionListEntry js_dnn_static_funcs[] = {
 
 extern "C" int
 js_dnn_init(JSContext* ctx, JSModuleDef* m) {
+  dnn_object = JS_NewObjectProto(ctx, JS_NULL);
 
   /* create the Net class */
   JS_NewClassID(&js_net_class_id);
@@ -502,21 +219,24 @@ js_dnn_init(JSContext* ctx, JSModuleDef* m) {
   JS_SetPropertyFunctionList(ctx, net_class, js_net_static_funcs, countof(js_net_static_funcs));
 
   // js_object_inspect(ctx, net_proto, js_net_inspect);
-}
 
-if(m) {
-  // JS_SetModuleExport(ctx, m, "Net", net_class);
-  //  JS_SetModuleExportList(ctx, m, js_net_global_funcs, countof(js_net_global_funcs));
+  if(m) {
+    JS_SetModuleExport(ctx, m, "dnn", dnn_object);
 
-  JS_SetModuleExportList(ctx, m, js_dnn_static_funcs, countof(js_dnn_static_funcs));
-}
+    JS_SetPropertyFunctionList(ctx, dnn_object, js_dnn_static_funcs, countof(js_dnn_static_funcs));
 
-return 0;
+    JS_SetPropertyStr(ctx, dnn_object, "Net", net_class);
+
+    //    JS_SetModuleExportList(ctx, m, js_dnn_static_funcs, countof(js_dnn_static_funcs));
+  }
+
+  return 0;
 }
 
 extern "C" void
 js_dnn_export(JSContext* ctx, JSModuleDef* m) {
-  JS_AddModuleExportList(ctx, m, js_dnn_static_funcs, countof(js_dnn_static_funcs));
+  JS_AddModuleExport(ctx, m, "dnn");
+  // JS_AddModuleExportList(ctx, m, js_dnn_static_funcs, countof(js_dnn_static_funcs));
 }
 
 #if defined(JS_CV_MODULE)
